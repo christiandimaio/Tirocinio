8a2f0550bcf250207aa5f80b09ace2a15c409d4b
Christian <dima9610@gmail.com>
2020-05-10 00:01:48 +0200

    Inizio lavoro di migrazione
    Step 1

--- webApp/react-frontend/src/App.js
+++ webApp/react-frontend/src/App.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 import ReactDOM from 'react-dom';
 import Sign_Up from './components/sign_up/sign_up';
 import {Box,Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+
+
 export default class App extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            visibleSection : "signUP",
+        }
+    }
+
+    changeView = (value) => {
+        this.setState({visibleSection:value});
+    }
+
     render(){
-        return(
-            <Box display="flex" width={1} height={1}>
-                <Grid container direction="row" alignItems="center" justify="center" style={{width:"50%",margin:"auto"}}>
-                    <Grid item ><Sign_Up/></Grid>     
-                </Grid>
-            </Box>
-            
        
-        );
+        switch(this.state.visibleSection){
+            case "signUP" :
+                return (
+                    
+                        <Box display="flex" bgcolor="black" justifyContent="center"
+                    alignItems="center"
+                    minHeight="100vh">
+                        <Container maxWidth="sm" >
+                                            <Sign_Up dadProps={this.state} >
+                                            </Sign_Up>
+                        </Container>
+                        </Box>
+                        );
+            case "logIn":
+                return (
+                    <Box display="flex" width={1} height={1}>
+                                <Grid container direction="row" alignItems="center" justify="center" style={{width:"50%",margin:"auto"}}>
+                                    <Grid item >
+                                        <Sign_Up dadProps={this.state} changeView={this.changeView()}>
+                                        </Sign_Up>
+                                    </Grid>     
+                                </Grid>
+                    </Box>
+                );
+            case "main":
+                    return(<div></div>);
+            default:
+                    return(<div></div>);
+
+            
+        }
+        
     }
 }
-
 ReactDOM.render(<App/>,document.getElementById('root'))
--- webApp/react-frontend/src/components/sign_up/sign_up.js
+++ webApp/react-frontend/src/components/sign_up/sign_up.js
@@ -169,8 +169,8 @@ export default class Sign_Up extends React.Component{
         const {error} = this.state;
         return (
             <div>
-                <div id="" class="">
-                    <Box mb={2}>
+                <Box border={3} borderColor="#3f51b5" borderRadius="2%" bgcolor="white">
+                    <Box display="flex">
                         <Grid container spacing={2} justify="flex-start" alignItems="flex-start">
                             <Grid item xl={4} xs={6}>
                             <Button
@@ -189,32 +189,32 @@ export default class Sign_Up extends React.Component{
                         justify="center"
                         alignItems="baseline"
                     >
-                        <Grid item lg={6} xs={6}>
+                        <Grid item xl={5} xs={5}>
                             <TextField id="nome_textfield" error={error.nome.state} onChange={(e) => this.handleChange(e,'nome')} label="Nome" variant="outlined" required fullWidth
                             helperText="*Campo Richiesto">
 
                             </TextField>
                         </Grid>
-                        <Grid item xl={6} xs={6}>
+                        <Grid item xl={5} xs={6}>
                             <TextField id="cognome_textfield" error={error.cognome.state} onChange={(e) => this.handleChange(e,'cognome')} label="Cognome" variant="outlined" required fullWidth
                             helperText="*Campo Richiesto">
 
                             </TextField>
                         </Grid>
                         
-                        <Grid item xl={12} xs={12}>
+                        <Grid item xl={11} xs={11}>
                             <TextField id="email_textfield" error={error.email.state} name="email" onChange={(e) => this.handleChange(e,'email')} label="Indirizzo Email" variant="outlined" required fullWidth
                              helperText={error.email.message}>
 
                             </TextField>
                         </Grid>
 
-                        <Grid item xl={6} xs={6}>
+                        <Grid item xl={5} xs={5}>
                             <TextField id="password_textfield" error={error.password.state} onChange={(e) => this.handleChange(e,'password')} type="text" label="Password" variant="outlined" required fullWidth
                             helperText={error.password.message}>
                             </TextField>
                         </Grid>
-                        <Grid item xl={6} xs={6}>
+                        <Grid item xl={5} xs={6}>
 
                             <TextField id="password_conferma_textfield"  error={error.password_conferma.state} onChange={(e) => this.handleChange(e,'password_conferma')} type="text" label="Conferma Password" variant="outlined" required fullWidth
                             helperText={error.password_conferma.message}>
@@ -224,34 +224,34 @@ export default class Sign_Up extends React.Component{
                     <Box mt={1}>
                         <Grid
                             container
-                            direction="column"
-                            justify="flex-start"
-                            alignItems="baseline"
+                            direction="row"
+                            justify="center"
+                            alignItems="center"
                             spacing={2}
                         >   
-                            <Grid mt={2} item xl={6} xs={6}>
-                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <KeyboardDatePicker
-                                margin="normal"
-                                id="date-picker-dialog"
-                                label="Date picker dialog"
-                                format="MM/dd/yyyy"
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change date',
-                                }}
-                            />
-                            </MuiPickersUtilsProvider>
+                            <Grid  mt={2} item xl={5} xs={5} spacing={2}>
+                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                    <KeyboardDatePicker
+                                        margin="normal"
+                                        id="date-picker-dialog"
+                                        label="Date picker dialog"
+                                        format="MM/dd/yyyy"
+                                        KeyboardButtonProps={{
+                                            'aria-label': 'change date',
+                                        }}
+                                    />
+                                </MuiPickersUtilsProvider>
                             </Grid>
-                            <Grid mt={2} item xl={10} xs={10} spacing={2}>
+                            <Grid mt={2} xl={7} xs={6} item  spacing={2}>
                             <Selecter
-                                        properties = {{labelId:"label-selecter-id",id:"selecter",inputLabel:"Tipo Utente",minWidth:250,style:operatore_style,value:this.state.tipo_utente,
+                                        properties = {{labelId:"label-selecter-id",id:"selecter",inputLabel:"Tipo Utente",minWidth:270,style:operatore_style,value:this.state.tipo_utente,
                                         customHandler:this.handleChange,helperText:error.tipo_utente.message,name:"tipo_utente",error:error.tipo_utente.state}}
                                         server_uri= '/database/select/user/type'>
                                         
                             </Selecter>
                                 
                             </Grid>
-                            <Grid item xl={8} xs={8}>
+                            <Grid item xl={11} xs={11}>
                             <TextField id="outlined-full-width" onChange={(e) => this.handleChange(e,'telefono_utente')} label="Telefono" variant="outlined" fullWidth
                             InputProps={{
                                 startAdornment: <InputAdornment position="start">+39</InputAdornment>,
@@ -269,7 +269,7 @@ export default class Sign_Up extends React.Component{
                         </Grid>
                     </Box>
                     
-                </div>
+                </Box>
                 <div> 
                     {
                         this.state.sign_in.called
