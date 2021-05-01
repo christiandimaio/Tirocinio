b00488958291360fb0f25c86e88bdd9615d31b91
Christian Di Maio <dima9610@gmail.com>
2021-05-01 22:19:48 +0200

    Risolto bug inserimento stazione
    Non era possibile inserire più di un responsabile per stazione:
    - Modificata definizione superchave di responsabile
    - Rimosso vincolo unique su stazione in responsabile

--- back-end/Model/Responsabile.py
+++ back-end/Model/Responsabile.py
@@ -3,8 +3,8 @@ from pony.orm import *
 from connection import db
 
 class Responsabile(db.Entity):
-    stazione_sismica = Required('Stazione_Sismica', unique=True, column='id_stazione')
+    stazione_sismica = Required('Stazione_Sismica', column='id_stazione')
     operatore = Required('Operatore', column='cod_operatore')
     data_fine_incarico = Optional(date)
     data_inizio_incarico = Required(date, default=lambda: date.today())
-    PrimaryKey(operatore, stazione_sismica)
+    PrimaryKey(operatore, stazione_sismica, data_inizio_incarico)
